c47e6525832ec7aab4c5bb58a97432478823e235
Nattapol Tassaneeyakul <pokneun@gmail.com>
2023-03-22 13:38:28 +0700

    update code

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -2,8 +2,8 @@
 
 class Constants{
 
-  // static const String baseUrl = "https://axgbd24uvh.execute-api.ap-southeast-1.amazonaws.com/dev";
-  static const String baseUrl = "https://ct6qxl2jc2.execute-api.ap-southeast-1.amazonaws.com/dev";
+  static const String baseUrl = "https://axgbd24uvh.execute-api.ap-southeast-1.amazonaws.com/dev";
+  // static const String baseUrl = "https://ct6qxl2jc2.execute-api.ap-southeast-1.amazonaws.com/dev";
   static const String tokenHeaderPrefix = "Bearer";
   static String? accessToken;
 }
